a3615bff91c0e310fe48a59643dea486a02801d9
Asa Aveanna <asa@holomaplefeline.net>
2019-12-01 17:48:58 -0800

    Log more things.

--- server.py
+++ server.py
@@ -1,38 +1,27 @@
-from logging import captureWarnings, getLogger
-from logging.handlers import SysLogHandler
-from typing import Union
+from logging import INFO, basicConfig, captureWarnings, getLogger
 from os import environ
+from typing import Union
 
-from discord import Member, Message, Reaction, User
+from discord import Member, Reaction, User
 from discord.ext.commands import Bot
 
 User = Union[Member, User]
-
+basicConfig(level=INFO)
 LOGGER = getLogger("fish.straycat.discordbot")
-LOGGER.addHandler(SysLogHandler())
 
 
 class PostFurryClient(Bot):
     async def on_error(self, event: str, *args: object, **kwargs: object) -> None:
         LOGGER.info("Error occured", event, *args, kwargs)
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         LOGGER.info("We have logged in as", self.user)
 
-    async def on_message(self, message: Message) -> None:
-        if not message.content.startswith("!rank "):
-            return
-
-        content = message.content[5:].strip()
-
-        if content.startswith("$hello"):
-            await message.channel.send("Hello!")
-
     async def on_reaction_add(self, reaction: Reaction, user: User) -> None:
-        pass
+        LOGGER.info(reaction, user)
 
     async def on_reaction_remove(self, reaction: Reaction, user: User) -> None:
-        pass
+        LOGGER.info(reaction, user)
 
 
 if __name__ == "__main__":
